cfc5d4e241cec089ec21ad6431b35e9fdcc75122
Mrinal banerjee <61612873+mrinalBanerjee@users.noreply.github.com>
2021-12-06 07:17:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-<link rel="stylesheet" href="style.css"></head>
+  
+<link rel="stylesheet" href="style.css">
+ </head>
  <body>
 <nav class="navbar navbar-dark bg-primary">
   <a class="navbar-brand" href="index.php">Banerjee Stores Department</a>
