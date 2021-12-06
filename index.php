19995a59e17748bc03bf075fe8374dbf389b77b8
Mrinal banerjee <61612873+mrinalBanerjee@users.noreply.github.com>
2021-12-06 07:16:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
-<?php
-if(isset($_POST["sub"]))
-{
-  $conn=mysqli_connect("localhost","root","","store");
-  $Depname=$_POST["Deptname"];
-  $item=$_POST["item"];
-  $quan=$_POST["quantity"];
-  $Add=$_POST["Address"];
-  $sql="insert into items(department,item,quartity,address) 
-  values('$Depname','$item',$quan,'$Add')";
 
-if (mysqli_query($conn,$sql))
-{
-
-  echo '<script type="text/javascript">';
-  echo ' alert("booking successful")'; 
-  echo '</script>'; 
-
-}
-else{
-  echo "not ok";
-  echo mysqli_error($conn);
-}
-}
-  
-
-?>
 <!DOCTYPE html>
 <html>
  <head>
