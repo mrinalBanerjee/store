3c83b67bc01f5190779f9768546a855eddd57e50
Mrinal banerjee <61612873+mrinalBanerjee@users.noreply.github.com>
2022-01-17 11:33:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,11 @@
  <body>
 <nav class="navbar navbar-dark bg-primary">
   <a class="navbar-brand" href="index.php">Banerjee Stores Department</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler btn btn-link" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="border-color:#007AFF;">
+
+    <span class="fas fa-bars" style="font-size:27px; color:#F5FFFF;"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
+	  <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
